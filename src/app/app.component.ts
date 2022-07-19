3e0a385adb162f3d4d58e70ed1f35a3ce29fbe8e
Simon Weiss <simonweiss.ml@gmail.com>
2022-07-19 17:49:14 +0200

    added textslide and main div

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'simonweiss.io';
+export class AppComponent implements OnInit {
+  textSlide = ['Have a great ', 'Be awesome this ', 'Stay happy this ', 'Have a wonderful ', 'Keep smiling this '];
+
+  currentText = 0;
+
+  ngOnInit() {
+    this.updateTextSlide();
+  }
+
+  updateTextSlide() {
+
+    setInterval(() => {
+      this.currentText++
+      this.currentText = this.currentText % this.textSlide.length;
+    }, 3000);
+
+  }
 
 
 
 
 
-  
 }
