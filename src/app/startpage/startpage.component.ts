c977905a6b82268a78792059c8515e60fad6012d
Simon Weiss <simonweiss.ml@gmail.com>
2022-07-21 22:46:40 +0200

    added navbar design

--- src/app/startpage/startpage.component.ts
+++ src/app/startpage/startpage.component.ts
@@ -6,23 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./startpage.component.scss']
 })
 export class StartpageComponent implements OnInit {
-
   textSlide = ['a great ', 'a beautiful ', 'a tiptop ', 'an excellent ', 'a remarkable ', 'an awesome', 'an exquisit', 'an unforgetable'];
-
   currentText = 0;
+  weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', ' Thursday', 'Friday', 'Saturday'];
+  day = new Date();
+  today = '';
+  number = 0;
 
   ngOnInit() {
     this.updateTextSlide();
+    this.updateDay();
+    window.addEventListener('scroll', this.scrollEvent, true);
+  }
+
+  scrollEvent = (event: any): void => {
+    const scroll = event.srcElement.scrollingElement.scrollTop;
+  let opa = document.getElementById('profile')?.style.opacity;
+      console.log('haöllo', opa);
+  
   }
 
   updateTextSlide() {
-
     setInterval(() => {
       this.currentText++
       this.currentText = this.currentText % this.textSlide.length;
     }, 3000);
-
   }
+
+  updateDay() {
+    this.today = this.weekday[this.day.getDay()];
+  }
+
+  
 
 
 }
