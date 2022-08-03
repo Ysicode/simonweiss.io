7e4ecabfc04c4c88e289363f34bbd111182faacf
Simon Weiss <simonweiss.ml@gmail.com>
2022-08-03 22:55:53 +0200

    added debugging

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent extends NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    super();
+  }
 
-  ngOnInit(): void {
+  override ngOnInit(): void {
   }
 
 }
